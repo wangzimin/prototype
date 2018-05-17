93ad238668ef753b61a75b59a996ce6d033137c0
黄伟(异势/服务研发中心) <wei.huang7@baidao.com>
2018-05-17 16:27:14 +0800

    Add new file

---
+++ src/components/Live/live.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="activeLive">
+        <div class="activeLiveWrap">
+            <div>
+                <videoWindow></videoWindow>
+                <!--<p></p>-->
+                <!--<navCenter></navCenter>-->
+                <!--<chooseNet></chooseNet>-->
+            </div>
+        </div>
+        <getLoad></getLoad>
+    </div>
+    </div>
+</template>
+<script>
+  import {mapGetters} from 'vuex'
+  import videoWindow from './video.vue'
+  import navCenter from './navCenter.vue'
+  import getLoad from "../common/getLoad.vue"
+  //  import chooseNet from "../common/chooseNet.vue"
+
+  export default {
+    name: 'Live',
+    data () {
+      return {
+      }
+    },
+    computed: {
+
+    },
+    created () {
+
+    },
+    components: {
+      videoWindow,
+      navCenter,
+      getLoad
+    },
+    destroyed(){
+    },
+    mounted () {
+
+    }
+  }
+</script>
+<style>
+    .activeLive {
+        height: 100%;
+        /*width: 1280px;*/
+        width: 100%;
+        /*background-color: #373e47;*/
+        display: flex;
+        flex: 9;
+        flex-direction: column;
+        margin: 0 auto;
+        padding: 0;
+    }
+    .activeLiveWrap{
+        /*border-bottom: 1px solid #e8e8e8;*/
+    }
+    .traininghome , .myhome{
+        border:none
+    }
+</style>
